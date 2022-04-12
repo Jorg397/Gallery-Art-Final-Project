ff21f8d771b9ea19771aa674bbe298330b5ccc60
Ivan Yoel Iraldi <87243361+ivaniraldi@users.noreply.github.com>
2022-04-12 17:56:16 -0300

    Update index.js

--- Front-end/src/redux/actions/index.js
+++ Front-end/src/redux/actions/index.js
@@ -1,8 +1,1 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducers";
-import thunk from "redux-thunk";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-export const store = createStore (
-    rootReducer, 
-    composeEnhancer(applyMiddleware(thunk)))
