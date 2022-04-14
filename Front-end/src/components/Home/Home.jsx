d243aafd7633871a52ffd820c737b1b643fb1649
Jorge Gonzalez <gonzalezsilva25795@gmail.com>
2022-04-14 14:46:58 -0400

    detailPage-Jorge

--- Front-end/src/components/Home/Home.jsx
+++ Front-end/src/components/Home/Home.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react'
-import About from '../About/About'
-import Card from '../Card/Card'
-import Destacado from '../Destacado/Destacado'
-import Footer from '../Footer/Footer'
-import Ilustrame from '../Ilustrame/Ilustrame'
-import Titulos from '../Titulos/Titulos'
-import { useAuth0 } from '@auth0/auth0-react'
-import Profile from '../Profile/Profile'
+import React, { useState } from "react";
+import About from "../About/About";
+import Destacado from "../Destacado/Destacado";
+import Footer from "../Footer/Footer";
+import Ilustrame from "../Ilustrame/Ilustrame";
+import Titulos from "../Titulos/Titulos";
+import { useAuth0 } from "@auth0/auth0-react";
+import Profile from "../Profile/Profile";
 
-export default function Home() { 
-  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();  
+import s from "./home.module.css";
+import Cards from "../Cards/Cards";
 
-    return (
-        <div>
-          <button onClick={()=> loginWithRedirect()}>Log-in</button>
-          {
-            isAuthenticated && <div>
-              <img src={user.picture} alt="Profile" />
-              <h2>{user.name}</h2>
-              <p>{user.email}</p>
-              <button onClick={() => logout()}>Log out</button>
-              </div>
-          }
+export default function Home() {
+	const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
 
-            <Titulos titulo="Cuadros decorativos"></Titulos>
-            <Destacado></Destacado>
-            <Titulos titulo="Una galeria explendida"></Titulos>
-            <Card></Card>
-            <Titulos titulo="Ilustramos tu personalidad"></Titulos>
-            <Ilustrame></Ilustrame>
-            <Titulos titulo="Sobre nosotros"></Titulos>
-            <About></About>
-            <Footer></Footer>
-        </div>
-    )
+	return (
+		<div className={s.container}>
+			<button onClick={() => loginWithRedirect()}>Log-in</button>
+			{isAuthenticated && (
+				<div>
+					<img src={user.picture} alt='Profile' />
+					<h2>{user.name}</h2>
+					<p>{user.email}</p>
+					<button onClick={() => logout()}>Log out</button>
+				</div>
+			)}
+
+			<Titulos titulo='Cuadros decorativos'></Titulos>
+			<Destacado></Destacado>
+			<Titulos titulo='Una galeria explendida'></Titulos>
+			<Cards />
+			<Titulos titulo='Ilustramos tu personalidad'></Titulos>
+			<Ilustrame></Ilustrame>
+			<Titulos titulo='Sobre nosotros'></Titulos>
+			<About></About>
+			<Footer></Footer>
+		</div>
+	);
 }
--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import {Provider} from 'react-redux';
-import {store} from '../src/redux/store';
-import {BrowserRouter} from 'react-router-dom';
-import {Auth0Provider} from '@auth0/auth0-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "../src/redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render (
-  <Provider store={store}>
-        <React.StrictMode>
-            <BrowserRouter>
-      <Auth0Provider domain='dev-flotws23.us.auth0.com' clientId='pRJYzAopJub2qstKEvPHdoDBOBuX1yx5'
-          redirectUri={
-              window.location.origin
-      }>
-                    <App/>
-                </Auth0Provider>
-            </BrowserRouter>
-        </React.StrictMode>
-    </Provider>,
-    document.getElementById('root')
+ReactDOM.render(
+	<Provider store={store}>
+		<React.StrictMode>
+			<BrowserRouter>
+				<Auth0Provider
+					domain='dev-flotws23.us.auth0.com'
+					clientId='pRJYzAopJub2qstKEvPHdoDBOBuX1yx5'
+					redirectUri={window.location.origin}>
+					<App />
+				</Auth0Provider>
+			</BrowserRouter>
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById("root")
 );
