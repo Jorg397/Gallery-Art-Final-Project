7f1d786f335acf28b7dd921af2f43b99a42cd80e
hagi torres <hagiraitorresmacedo@gmail.com>
2022-04-15 14:19:36 -0500

    corrigiendo conflicto de regristro aver si funciona

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,6 +1,9 @@
 import Home from "./components/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./components/Detail/Detail";
+import Login from "./Page/Login/index";
+import Registry from "./Page/Registry/index";
+import './App.css';
 import Landing from "./components/Landing/Landing";
 
 function App() {
