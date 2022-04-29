ac1dc9bfa4bf32fbdb17162ec205a030a4eaeb80
ShitoinTokyo-3 <juanmanuelduque0920@gmail.com>
2022-04-28 20:16:22 -0500

    juand-back-Proteccion-de-rutas-dia28

--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -2,8 +2,16 @@ const router = require("express").Router();
 const passport = require("passport");
 
 const orders = require("./controllers/orders");
+const { checkRoles } = require("./utils/models/models");
 
-router.route("/").get(orders.get);
-router.route("/:idCustomer").get(orders.getCustomer);
+router.get("/", 
+    passport.authenticate("jwt", { session: false }),
+    checkRoles( "admin"),
+    orders.get);
+
+router.get("/:idCustomer",
+    passport.authenticate("jwt", { session: false }),
+    checkRoles( "admin", "user"),
+    orders.getCustomer);
 
 module.exports = router;
--- api/src/routes/payment.js
+++ api/src/routes/payment.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const paymentControllers = require("./controllers/payment.js");
 const passport = require('passport');
+const { checkRoles } = require("./utils/models/models.js");
 
 //prettier-ignore
 router.post("/",
     passport.authenticate('jwt', { session: false }),
+    checkRoles('admin', 'user'),
     paymentControllers.post);
-// router.route('/')
-//     .post(paymentControllers.post);
+
 module.exports = router;
