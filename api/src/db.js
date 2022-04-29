9698125ed3371204661e9c4d69e230d0270a3424
Juan Pablo Tuttolomondo <jptuttolomondo@hotmail.com>
2022-04-29 12:12:57 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -15,27 +15,23 @@ const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+
 modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
+
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
 const { Product,Seller,Order,Customer,Category,Comment,Image } = sequelize.models;
 
 
-// Aca vendrian las relaciones
-//Product.hasMany(Reviews);
+
 Seller.hasMany(Product);
 Product.belongsTo(Seller);
 Customer.hasMany(Order);
@@ -47,10 +43,6 @@ Category.belongsToMany(Product,{through:"product_category"})
 Comment.hasMany(Image)
 Image.belongsTo(Comment)
 
-//User.hasOne(Address, { as: "domicilio", foreignKey: "residente_id" });
-
-// Añade una clave userId a la tabla addresses
-//Address.belongsTo(User, { as: "residente", foreignKey: "residente_id" });
 Order.hasOne(Comment)
 Comment.belongsTo(Order);
 
