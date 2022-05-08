f39f6e64c32e636959ed95ef64b6085a5ad0b857
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-05-07 21:16:15 -0500

    fix url deploy

--- Front-end/src/services/post/login.js
+++ Front-end/src/services/post/login.js
@@ -5,6 +5,6 @@ const apiUrl = import.meta.env.VITE_URL_API;
 export const login = async (customer) => {
   console.log({ customer });
   const { email, password } = customer;
-  let url = `${apiUrl}customer/login`;
+  let url = `${apiUrl}/customer/login`;
   return await axios.post(url, { email, password });
 };
--- Front-end/src/services/post/registry.js
+++ Front-end/src/services/post/registry.js
@@ -5,6 +5,6 @@ const apiUrl = import.meta.env.VITE_URL_API;
 export const createCustomer = async (customer) => {
   const { email, password } = customer;
 
-  let url = `${apiUrl}customer/create`;
+  let url = `${apiUrl}/customer/create`;
   return await axios.post(url, { email, password });
 };
